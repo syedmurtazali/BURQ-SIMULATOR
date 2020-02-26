db4ea801cbed84d79e3bf52c03cbc940072f6cce
Muhammadwaleed99 <57762838+Muhammadwaleed99@users.noreply.github.com>
2020-02-26 14:28:28 +0500

    Add files via upload

---
+++ Regression_Test/Test_QuickSort/Test_QuickSort.c
@@ -0,0 +1,43 @@
+ 
+void quicksort (int [], int, int);
+ 
+void main()
+{
+    int list[6]={12,32,11,43,12};
+    int size=5, i;
+ 
+    quicksort(list, 0, size - 1);
+ 
+}
+void quicksort(int list[], int low, int high)
+{
+    int pivot, i, j, temp;
+    if (low < high)
+    {
+        pivot = low;
+        i = low;
+        j = high;
+        while (i < j) 
+        {
+            while (list[i] <= list[pivot] && i <= high)
+            {
+                i++;
+            }
+            while (list[j] > list[pivot] && j >= low)
+            {
+                j--;
+            }
+            if (i < j)
+            {
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+        }
+        temp = list[j];
+        list[j] = list[pivot];
+        list[pivot] = temp;
+        quicksort(list, low, j - 1);
+        quicksort(list, j + 1, high);
+    }
+}
